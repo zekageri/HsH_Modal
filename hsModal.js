0c757c49a78b62d9a80a629ec6a0ebfef6e2a843
DrRandom <37899881+zekageri@users.noreply.github.com>
2022-09-01 14:13:39 +0200

    Create hsModal.js

---
+++ hsModal.js
@@ -0,0 +1,112 @@
+class hsModal extends HTMLElement {
+    openBtn = null;
+    closeBtn = null;
+    id = null;
+    closeCb = null;
+    openCb = null;
+    innerDiv = null;
+
+    lastScrollPos = 0;
+    MAX_SCROLL_POS = 150;
+
+    connectedCallback() {
+        this.id = this.getAttribute("id");
+        if (!this.id) {
+            console.error("[HSH-Modal] - Modal should have an ID.");
+            return;
+        }
+        this.openBtn = document.querySelector(`.hs-Modal-Btn.open[target="#${this.id}"]`);
+        this.closeBtn = this.querySelector(`.hs-Modal-Btn.close`);
+        this.innerDiv = this.querySelector(".hs-Modal-Inner");
+
+        this.openEvtHandler();
+        this.closeEvtHandler();
+        this.scrollEvtHandler();
+    }
+
+    scrollEvtHandler() {
+        let headNav = this.querySelector(".hs-Modal-Header");
+        let self = this;
+        this.innerDiv.addEventListener("scroll", function (e) {
+            let scrollTop = this.scrollTop;
+            if (scrollTop >= self.MAX_SCROLL_POS) {
+                if (scrollTop < self.lastScrollPos) {
+                    headNav.classList.remove("scrolled");
+                } else if (!headNav.classList.contains("scrolled")) {
+                    headNav.classList.add("scrolled");
+                }
+            } else if (scrollTop < self.MAX_SCROLL_POS) {
+                headNav.classList.remove("scrolled");
+            }
+            self.lastScrollPos = scrollTop;
+        });
+    };
+
+    on(type, callback) {
+        if (type == "close") {
+            this.closeCb = callback;
+        } else if (type == "open") {
+            this.openCb = callback;
+        } else {
+            console.warn(`[HSH-Modal] - No such event type on HsH Modal. Event: ${type}`);
+        }
+    }
+
+    openEvtHandler() {
+        let self = this;
+        this.openBtn.addEventListener("click", function (e) {
+            e.preventDefault();
+            self.open();
+        });
+    }
+
+    closeEvtHandler() {
+        let self = this;
+        // Close if close button clicked.
+        this.closeBtn.addEventListener("click", function (e) {
+            e.preventDefault();
+            self.close();
+        });
+        // Close if clicked outside the modal
+        this.addEventListener("click", function (e) {
+            let modalInner = e.target.closest(".hs-Modal-Inner");
+            if (!modalInner) {
+                self.close();
+            }
+        });
+    }
+
+    close(shouldCB = true) {
+        if (this.closeCb && shouldCB) { this.closeCb(this.id); }
+        this.classList.remove("open");
+    }
+
+    open(shouldCB = true) {
+        if (this.openCb && shouldCB) { this.openCb(this.id); }
+        this.classList.add("open");
+    }
+};
+customElements.define('hs-modal', hsModal);
+
+
+/************************* TEST BELOW *************************
+ 
+let testModal = document.querySelector("#testModal");
+ 
+testModal.on("open",function(id){
+  console.log(`Modal opened with id: ${id}`);
+});
+testModal.on("close",function(id){
+  console.log(`Modal closed with id: ${id}`);
+});
+ 
+testModal.on("noSuchEvent",function(id){
+  //no such event
+});
+ 
+testModal.open();
+setTimeout(function(){
+  testModal.close();
+}, 3000);
+ 
+*/
